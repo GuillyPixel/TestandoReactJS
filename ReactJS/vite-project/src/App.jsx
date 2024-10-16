61517c7542248627e23aad40194b1c11d07bdfd6
Guilherme Martins Fernandes <guillherme10mar@gmail.com>
2024-10-16 17:36:47 -0300

    pt01

---
+++ ReactJS/vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+
+import Sampa from './assets/imagens/Sampaa.png'
+import Morumbi from './assets/imagens/tri.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+        <a href="https://react.dev" target="_blank">
+          <img src={Sampa} className= "sampao" alt="Maior do Brasil" />
+          <img src={Morumbi} className= "morumbis" alt="O Mais Popular" />
+        </a>
+      </div>
+      <h1>Testando Primeiro APP em REACTJs</h1>
+     
+    
+    </>
+  )
+}
+
+export default App
